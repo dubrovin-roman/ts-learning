9cbabefd9294ca140c5b610af978ea36fc571fd7
Roman Dubrovin <dubrovin.skfo@gmail.com>
2023-07-28 16:11:17 +0300

    28072023

--- app.js
+++ app.js
@@ -22,4 +22,23 @@ function getFullNameFromUserEntity(userEntity) {
 }
 console.log(getFullNameFromUserEntity(user));
 // ------------------ МАССИВЫ ---------------------------
-const skills = ["dev", "devops"];
+const skills = ["dev", "devops", "testing"];
+skills.forEach((skill) => console.log(skill.toLocaleUpperCase()));
+const res1 = skills
+    .filter((s) => s !== "dev")
+    .map((s) => s.toLocaleUpperCase() + " ")
+    .reduce((s1, s2) => s1 + s2)
+    .trim();
+console.log(res1);
+// ----------------- TUPLES (КОРТЕЖИ) ---------------------
+const skill = [1, "dev"];
+const [id, skillName] = skill;
+console.log(id);
+console.log(skillName);
+const arr = [1, "dev", true, true, false];
+// --------------------- readonly for array and tuples ----------------------- //
+const skill2 = [1, "dev"];
+const arr1 = [1, 2, 3];
+// используем джинерик
+const skills2 = ["dev", "devops"];
+const skills3 = ["dev", "devops"];
--- app.ts
+++ app.ts
@@ -35,4 +35,33 @@ console.log(getFullNameFromUserEntity(user));
 
 // ------------------ МАССИВЫ ---------------------------
 
-const skills: string[] = ["dev", "devops"];
+const skills: string[] = ["dev", "devops", "testing"];
+
+skills.forEach((skill) => console.log(skill.toLocaleUpperCase()));
+
+const res1 = skills
+  .filter((s) => s !== "dev")
+  .map((s) => s.toLocaleUpperCase() + " ")
+  .reduce((s1, s2) => s1 + s2)
+  .trim();
+
+console.log(res1);
+
+// ----------------- TUPLES (КОРТЕЖИ) ---------------------
+
+const skill: [number, string] = [1, "dev"];
+const [id, skillName] = skill;
+console.log(id);
+console.log(skillName);
+
+const arr: [number, string, ...boolean[]] = [1, "dev", true, true, false];
+
+// --------------------- readonly for array and tuples ----------------------- //
+
+const skill2: readonly [number, string] = [1, "dev"];
+const arr1: readonly number[] = [1, 2, 3];
+
+// используем джинерик
+
+const skills2: Array<string> = ["dev", "devops"];
+const skills3: ReadonlyArray<string> = ["dev", "devops"];
