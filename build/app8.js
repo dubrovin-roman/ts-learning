6f40102de955290e3dfa9e0eaba60d3f0f775a9c
Roman Dubrovin <dubrovin.skfo@gmail.com>
2023-08-07 17:41:36 +0300

    вырожения в типах

--- build/app8.js
+++ build/app8.js
@@ -9,3 +9,60 @@ function getValue(obj, key) {
     return obj[key];
 }
 console.log(getValue(user8, "name"));
+//8.3 Упражнение - Пишем функцию группировки
+const data8 = [
+    { group: 1, name: "b" },
+    { group: 1, name: "a" },
+    { group: 2, name: "c" },
+];
+function groupBy(data, key) {
+    var _a;
+    const resultObj = {};
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        if (!element)
+            continue;
+        const value = `${element[key]}`;
+        value in resultObj
+            ? (_a = resultObj[value]) === null || _a === void 0 ? void 0 : _a.push(element)
+            : (resultObj[value] = [element]);
+    }
+    return resultObj;
+}
+console.log(groupBy(data8, "name"));
+// 8.4 Typeof
+let strOrNumb;
+if (Math.random() > 0.5) {
+    strOrNumb = 5;
+}
+else {
+    strOrNumb = "str";
+}
+if (typeof strOrNumb === "string") {
+    console.log(strOrNumb);
+}
+else if (typeof strOrNumb === "number") {
+    console.log(strOrNumb);
+}
+let strOrNumb2;
+// typeof & keyof
+const user81 = {
+    name: "Roman",
+    age: 37,
+    city: "Kislovodsk",
+};
+var Direction;
+(function (Direction) {
+    Direction[Direction["UP"] = 0] = "UP";
+    Direction[Direction["DOWN"] = 1] = "DOWN";
+    Direction[Direction["LEFT"] = 2] = "LEFT";
+    Direction[Direction["RIGHT"] = 3] = "RIGHT";
+})(Direction || (Direction = {}));
+const direcUp = Direction["UP"];
+const user82 = {
+    name: "Roman",
+    roles: [{ nameRole: "Admin" }],
+};
+const userName = user82["name"];
+// преобразование массива с значениями к типу с летералами этих значений
+const roles = ["user", "admin", "super-user"];
--- src/app8.ts
+++ src/app8.ts
@@ -14,15 +14,108 @@ const user8: IUser = {
 type KeysOfIuser = keyof IUser;
 
 function getValue<T, K extends keyof T>(obj: T, key: K) {
-    return obj[key];
+  return obj[key];
 }
 
 console.log(getValue(user8, "name"));
 
 //8.3 Упражнение - Пишем функцию группировки
 
-function groupBy<T, K extends keyof T>(data: T[], key: K) {
-    const resultObj = {};
-    
-    return resultObj;
+const data8 = [
+  { group: 1, name: "b" },
+  { group: 1, name: "a" },
+  { group: 2, name: "c" },
+];
+
+function groupBy<T, K extends keyof T>(data: T[], key: K): Record<string, T[]> {
+  const resultObj: Record<string, T[]> = {};
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    if (!element) continue;
+    const value: string = `${element[key]}`;
+
+    value in resultObj
+      ? resultObj[value]?.push(element)
+      : (resultObj[value] = [element]);
+  }
+  return resultObj;
+}
+
+console.log(groupBy(data8, "name"));
+
+// 8.4 Typeof
+
+let strOrNumb: string | number;
+
+if (Math.random() > 0.5) {
+  strOrNumb = 5;
+} else {
+  strOrNumb = "str";
+}
+
+if (typeof strOrNumb === "string") {
+  console.log(strOrNumb);
+} else if (typeof strOrNumb === "number") {
+  console.log(strOrNumb);
+}
+
+let strOrNumb2: typeof strOrNumb;
+
+// typeof & keyof
+
+const user81 = {
+  name: "Roman",
+  age: 37,
+  city: "Kislovodsk",
+};
+
+type keyOfUser = keyof typeof user81;
+
+enum Direction {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+type direc = keyof typeof Direction;
+
+const direcUp = Direction["UP"];
+
+// 8.5 Indexed Access Types
+
+interface IRole8 {
+  nameRole: string;
+}
+
+interface IUser8 {
+  name: string;
+  roles: IRole8[];
+}
+
+const user82: IUser8 = {
+  name: "Roman",
+  roles: [{ nameRole: "Admin" }],
+};
+
+const userName = user82["name"];
+
+type rolesType = IUser8["roles"];
+
+// обращение к элементу массива типов
+
+type roleType = IUser8["roles"][number];
+
+// преобразование массива с значениями к типу с летералами этих значений
+
+const roles = ["user", "admin", "super-user"] as const;
+
+type roleTypes = typeof roles[number];
+
+// 8.6 Conditional Types
+
+interface HTTPResponse<T extends "success" | "failed"> {
+  code: number;
+  data: T extends "success" ? string : Error;
+  data2: T extends "success" ? string : number;
 }
