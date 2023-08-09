f423ecf1356556ab8626beb970144ae6f55061b1
Roman <dubrovin.skfo@gmail.com>
2023-08-09 22:30:27 +0300

    декоратор метода

--- build/decorator.js
+++ build/decorator.js
@@ -5,6 +5,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 //@nullUsersNum
 // @threeUsersAdvanced
 // @SetUsers(15)
@@ -34,8 +43,9 @@ function threeUsersAdvanced(constructor) {
     };
 }
 const userService = new UserService();
-console.log(userService.getUsersNumInDB());
-console.log(userService);
+// console.log(userService.getUsersNumInDB());
+// console.log(userService);
+// console.log((userService as IUserService & CreatedAt).CreatedAt);
 // фабрика
 function SetUsers(numUsers) {
     return (target) => {
@@ -61,3 +71,63 @@ function CreatedAt(constructor) {
         }
     };
 }
+// 10.6 Декоратор метода
+class UserServiceNew {
+    // @Log
+    getUsersNumInDB() {
+        throw new Error("Method not implemented.");
+    }
+}
+__decorate([
+    LogError({ reThrow: true })
+], UserServiceNew.prototype, "getUsersNumInDB", null);
+function Log(target, propertyKey, descriptor) {
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+    descriptor.value = () => {
+        console.log("no error");
+    };
+}
+// 10.7 Упражнение - Декоратор перехвата ошибок
+// для асинхронной функции
+function LogErrorAsync({ reThrow } = { reThrow: false }) {
+    return (target, _, descriptor) => {
+        const originalFun = descriptor.value;
+        descriptor.value = (...args) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                if (!originalFun)
+                    throw new Error("original function undefined");
+                return yield originalFun.apply(target, args);
+            }
+            catch (error) {
+                if (error instanceof Error) {
+                    console.log(error.message);
+                    if (reThrow)
+                        throw error;
+                }
+            }
+        });
+    };
+}
+// для обычной функции
+function LogError({ reThrow } = { reThrow: false }) {
+    return (target, _, descriptor) => {
+        const originalFun = descriptor.value;
+        descriptor.value = (...args) => {
+            try {
+                if (!originalFun)
+                    throw new Error("original function undefined");
+                return originalFun.apply(target, args);
+            }
+            catch (error) {
+                if (error instanceof Error) {
+                    console.log(error.message);
+                    if (reThrow)
+                        throw error;
+                }
+            }
+        };
+    };
+}
+new UserServiceNew().getUsersNumInDB();
--- src/decorator.ts
+++ src/decorator.ts
@@ -3,6 +3,10 @@ interface IUserService {
   getUsersNumInDB(): number;
 }
 
+type CreatedAt = {
+  CreatedAt: Date;
+};
+
 //@nullUsersNum
 // @threeUsersAdvanced
 // @SetUsers(15)
@@ -30,8 +34,9 @@ function threeUsersAdvanced<T extends { new (...args: any[]): {} }>(
 }
 const userService = new UserService();
 
-console.log(userService.getUsersNumInDB());
-console.log(userService);
+// console.log(userService.getUsersNumInDB());
+// console.log(userService);
+// console.log((userService as IUserService & CreatedAt).CreatedAt);
 
 // фабрика
 
@@ -50,8 +55,80 @@ function SetUsersAdvanced(numUsers: number) {
 }
 
 // 10.5 Упражнение - Декоратор CreatedAt
-function CreatedAt<T extends {new(...args: any[]):{}}>(constructor: T) {
-    return class extends constructor {
-        CreatedAt = new Date();
-    }
+function CreatedAt<T extends { new (...args: any[]): {} }>(constructor: T) {
+  return class extends constructor {
+    CreatedAt = new Date();
+  };
 }
+
+// 10.6 Декоратор метода
+
+class UserServiceNew implements IUserService {
+  usersNumber: number;
+
+  // @Log
+  @LogError({ reThrow: true })
+  getUsersNumInDB(): number {
+    throw new Error("Method not implemented.");
+  }
+}
+
+function Log(
+  target: Object,
+  propertyKey: string | symbol,
+  descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+): TypedPropertyDescriptor<(...args: any[]) => any> | void {
+  console.log(target);
+  console.log(propertyKey);
+  console.log(descriptor);
+  descriptor.value = () => {
+    console.log("no error");
+  };
+}
+
+// 10.7 Упражнение - Декоратор перехвата ошибок
+// для асинхронной функции
+function LogErrorAsync({ reThrow }: { reThrow: boolean } = { reThrow: false }) {
+  return (
+    target: Object,
+    _: string | symbol,
+    descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+  ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
+    const originalFun = descriptor.value;
+    descriptor.value = async (...args: any[]) => {
+      try {
+        if (!originalFun) throw new Error("original function undefined");
+        return await originalFun.apply(target, args);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+          if (reThrow) throw error;
+        }
+      }
+    };
+  };
+}
+
+// для обычной функции
+function LogError({ reThrow }: { reThrow: boolean } = { reThrow: false }) {
+  return (
+    target: Object,
+    _: string | symbol,
+    descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+  ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
+    const originalFun = descriptor.value;
+    descriptor.value = (...args: any[]) => {
+      try {
+        if (!originalFun) throw new Error("original function undefined");
+        return originalFun.apply(target, args);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+          if (reThrow) throw error;
+        }
+      }
+    };
+  };
+}
+
+new UserServiceNew().getUsersNumInDB();
